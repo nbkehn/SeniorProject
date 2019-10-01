65d022b230c0dd484923ec7570b0a1d65525281f
Connor J. Parke <cjparke@ncsu.edu>
2019-10-01 01:00:06 -0400

    abtracted technicians to use abstract person

---
+++ server/src/main/java/bco/scheduler/model/AbstractPerson.java
@@ -0,0 +1,62 @@
+package bco.scheduler.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "technician")
+public abstract class AbstractPerson {
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name", nullable = true)
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = true)
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "phone", nullable = true)
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Column(name = "email", nullable = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
