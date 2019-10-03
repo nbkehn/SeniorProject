48447cf083b277f0a35676ec24f2e2a459ab3b55
Connor J. Parke <cjparke@ncsu.edu>
2019-10-03 01:39:24 -0400

    Revert "abtracted technicians to use abstract person"
    This reverts commit 65d022b230c0dd484923ec7570b0a1d65525281f.

--- server/src/main/java/bco/scheduler/model/Technician.java
+++ server/src/main/java/bco/scheduler/model/Technician.java
@@ -9,9 +9,18 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "technician")
-public class Technician extends AbstractPerson {
+public class Technician {
 
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
     private boolean inHouse;
+
+    public Technician() {
+
+    }
 
     public Technician(String firstName, String lastName, String email, String phone, boolean inHouse) {
         this.firstName = firstName;
@@ -19,6 +28,49 @@ public class Technician extends AbstractPerson {
         this.email = email;
         this.phone = phone;
         this.inHouse = inHouse;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name = "first_name", nullable = true)
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = true)
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Column(name = "phone", nullable = true)
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Column(name = "email", nullable = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Column(name = "in_house", nullable = true)
