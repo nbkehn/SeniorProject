2f2c9f0e0006456fbe52a989d7dd4bad74412186
Noah Trimble <nmtrimbl@ncsu.edu>
2019-10-18 10:15:04 -0400

    Added model and repository classes for appointment reminders

---
+++ server/src/main/java/bco/scheduler/model/Reminder.java
@@ -0,0 +1,113 @@
+package bco.scheduler.model;
+
+import javax.persistence.*;
+
+
+/**
+ * Reminder object, holds information related to reminders
+ * @author Noah Trimble
+ */
+@Entity
+public class Reminder {
+
+    /** reminder id */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    /** time to send reminder */
+    @Enumerated
+    @Column(name = "time_to_send", nullable = false, unique = true)
+    private TimeToSend timeToSend;
+
+    /** chosen email template */
+    @OneToOne
+    @JoinColumn(name="text_template_id", nullable = false)
+    @MapsId
+    private Template textTemplate;
+
+    /** chosen email template */
+    @OneToOne
+    @JoinColumn(name="email_template_id", nullable = false)
+    @MapsId
+    private Template emailTemplate;
+
+    /** default constructor */
+    public Reminder() {}
+
+    /**
+     * Reminder constructor
+     * @param timeToSend time to send reminder
+     * @param textTemplate text template to use
+     * @param emailTemplate email template to use
+     */
+    public Reminder(TimeToSend timeToSend, Template textTemplate, Template emailTemplate) {
+        this.timeToSend = timeToSend;
+        this.textTemplate = textTemplate;
+        this.emailTemplate = emailTemplate;
+    }
+
+    /**
+     * gets id
+     * @return id
+     */
+    public long getId() {
+        return id;
+    }
+    
+    /**
+     * sets id
+     * @param id ID
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * Get time to send
+     * @return time to send
+     */
+    public TimeToSend getTimeToSend() {
+        return timeToSend;
+    }
+
+    /**
+     * Set time to send
+     * @param timeToSend time to send
+     */
+    public void setTimeToSend(TimeToSend timeToSend) {
+        this.timeToSend = timeToSend;
+    }
+
+    /**
+     * Get text template
+     * @return text template
+     */
+    public Template getTextTemplate() {
+        return textTemplate;
+    }
+
+    /**
+     * Set text template
+     * @param textTemplate text template
+     */
+    public void setTextTemplate(Template textTemplate) {
+        this.textTemplate = textTemplate;
+    }
+
+    /**
+     * Get email template
+     * @return email template
+     */
+    public Template getEmailTemplate() {
+        return emailTemplate;
+    }
+
+    /**
+     * Set email template
+     * @param emailTemplate email template
+     */
+    public void setEmailTemplate(Template emailTemplate) {
+        this.emailTemplate = emailTemplate;
+    }
+}
