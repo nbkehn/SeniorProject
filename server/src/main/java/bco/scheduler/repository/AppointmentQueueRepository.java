67754c6e3b79f111a9174ee0967d87a654b21dcf
Noah Trimble <nmtrimbl@ncsu.edu>
2019-11-06 22:45:45 -0500

    Alter appointmetn queue query

--- server/src/main/java/bco/scheduler/repository/AppointmentQueueRepository.java
+++ server/src/main/java/bco/scheduler/repository/AppointmentQueueRepository.java
@@ -22,7 +22,7 @@ public interface AppointmentQueueRepository extends JpaRepository<AppointmentQue
             "SELECT aq.* FROM appointment_queue aq\n" +
             "LEFT JOIN appointment a ON a.id = aq.appointment_id\n" +
             "LEFT JOIN reminder r ON r.id = aq.reminder_id\n" +
-            "WHERE aq.is_sent = FALSE\n" +
+            "WHERE (aq.is_sent IS NULL OR aq.is_sent = FALSE)\n" +
             "AND (a.start_date_time + INTERVAL r.time_to_send DAY) <= NOW()", nativeQuery = true)
     Collection<AppointmentQueue> getReadyToSend();
 
