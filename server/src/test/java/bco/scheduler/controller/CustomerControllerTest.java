d1e225316b2ac7f6dea101ad02b9155043c31020
Will Duke <wfduke@ncsu.edu>
2019-12-05 14:30:04 -0500

    Finished CustomerControllerTest.java. All six tests pass, covering the methods from the controller.

--- server/src/test/java/bco/scheduler/controller/CustomerControllerTest.java
+++ server/src/test/java/bco/scheduler/controller/CustomerControllerTest.java
@@ -1,13 +1,10 @@
-/**
- * Tests the customer controller class.
- * 
- * @author Will Duke
- */
 package bco.scheduler.controller;
 import bco.scheduler.repository.CustomerRepository;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.junit.runner.RunWith;
 import org.junit.Test;
+import java.util.Map;
+import java.util.ArrayList;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
@@ -30,101 +27,183 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+/**
+ * Tests the customer controller class.
+ * Uses Mockiato for mocking of relevant pieces. 
+ * 
+ * @author Will Duke
+ */
+@RunWith(MockitoJUnitRunner.Silent.class)
 public class CustomerControllerTest {
 
-    @Autowired
-    private MockMvc mvc;
-
+    // A mock of the customer controller class.
     @Mock
     private CustomerController customerController = new CustomerController();
 
+    /**
+     * Tests the getting of all the customers through the
+     * getAllCustomers method.
+     */
     @Test
     public void getAllCustomersTest() throws Exception {
      
+        // A customer object to use.
         Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
 
+        // A customer object list to use.
         List<Customer> allCustomers = Arrays.asList(testCustomer);
  
+        // Response Entity representation of the desired output
         ResponseEntity<List<Customer>> re = ResponseEntity.ok(allCustomers);
 
+        // Ensure the customers are null.
         assertNull(customerController.getAllCustomers());
 
+        // Telling Mockiato how to handle the methods.
         when(customerController.getAllCustomers()).thenReturn(re);
 
+        // Ensure the customer is in there. 
         assertEquals(customerController.getAllCustomers(), re);
     }
 
-    // @Test
-    // public void getCustomerByIdTest() throws Exception {
+    /**
+     * Tests the getting of the customers by their id through
+     * the getCustomerById method.
+     */
+    @Test
+    public void getCustomerByIdTest() throws Exception {
+
+        // A customer object to use.
+        Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
+
+        // A customer object list to use.
+        List<Customer> allCustomers = Arrays.asList(testCustomer);
+ 
+        // Response Entity representation of the desired output
+        ResponseEntity<Customer> re = ResponseEntity.ok(testCustomer);
+
+        // Ensure there are no customers when they aren't in there.
+        assertNull(customerController.getCustomerById((long) testCustomer.getId()));
+
+        // Telling Mockiato how to handle the methods.
+        when(customerController.getCustomerById((long) testCustomer.getId())).thenReturn(re);
+
+        // Ensuring the customer can be found. 
+        assertEquals(customerController.getCustomerById((long) testCustomer.getId()), re);
+        
+    }
+
+    /**
+     * Tests that the customer is created successfully through 
+     * the create customer method.
+     */
+    @Test
+    public void createCustomerTest() throws Exception {
+        
+        // A customer object to use.
+        Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
+ 
+        // Response Entity representation of the desired output
+        ResponseEntity<Customer> re = ResponseEntity.ok(testCustomer);
+
+        // Telling Mockiato how to handle the methods.
+        when(customerController.createCustomer(testCustomer)).thenReturn(re);
+
+        // Ensuring the customer is created 
+        assertEquals(customerController.createCustomer(testCustomer), re);
+
+    }
+
+    /**
+     * Tests that the customers are updated properly through the
+     * update customer method.
+     */
+    @Test
+    public void updateCustomerTest() throws Exception {
      
-    //     Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
+        // A customer object to use.
+        Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
 
-    //     List<Customer> allCustomers = Arrays.asList(testCustomer);
+        // A customer object to update to. 
+        Customer testCustomerUpdated = new Customer("Jane", "Doe", "jdoe@gmail.com", "336-344-0576", CommunicationType.TEXT, "990 Oval Dr, Raleigh, NC 27606");
  
-    //     given(customerController.getAllCustomers()).willReturn(allCustomers);
- 
-    //     mvc.perform(get("/customers/1")
-    //         .contentType(MediaType.APPLICATION_JSON))
-    //         .andExpect(status().isOk())
-    //         .andExpect(jsonPath("$", hasSize(1)))
-    //         .andExpect(jsonPath("$[0].name", is(testCustomer.getFirstName())));
-    // }
+        // Response Entity representation of the desired output
+        ResponseEntity<Customer> re = ResponseEntity.ok(testCustomer);
 
-    // @Test
-    // public void createCustomerTest() throws Exception {
+        // Response Entity representation of the desired output
+        ResponseEntity<Customer> updatedRe = ResponseEntity.ok(testCustomerUpdated);
+
+        // Telling Mockiato how to handle the methods.
+        when(customerController.createCustomer(testCustomer)).thenReturn(re);
+
+        // Ensuring the customer is create
+        assertEquals(customerController.createCustomer(testCustomer), re);
+
+        // Telling Mockiato how to handle the methods.
+        when(customerController.updateCustomer(testCustomer.getId(), testCustomerUpdated)).thenReturn(updatedRe);
+
+        // Ensure the customer is updated correctly. 
+        assertEquals(customerController.updateCustomer(testCustomer.getId(), testCustomerUpdated), updatedRe);
+
+    }
+
+    /**
+     * Tests that customers are deleted using the delete customer method.
+     */
+    @Test
+    public void deleteCustomerTest() throws Exception {
+
+        // A customer object to use.
+        Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
+
+        // A customer object to use.
+        Customer testCustomerUpdated = new Customer("Jane", "Doe", "jdoe@gmail.com", "336-344-0576", CommunicationType.TEXT, "990 Oval Dr, Raleigh, NC 27606");
+ 
+        // Response Entity representation of the desired output
+        ResponseEntity<Customer> re = ResponseEntity.ok(testCustomer);
+        
+        // Response Entity representation of the desired output
+        ResponseEntity<Customer> deletedRe = ResponseEntity.ok(testCustomer);
+
+        // Telling Mockiato how to handle the methods.
+        when(customerController.createCustomer(testCustomer)).thenReturn(re);
+
+        // Telling Mockiato how to handle the methods.
+        when(customerController.deleteCustomer(testCustomer.getId())).thenReturn(deletedRe);
+
+        // Ensure that the customer is created. 
+        assertEquals(customerController.createCustomer(testCustomer), re);
+
+        // Ensure the customer is deleted. 
+        assertEquals(customerController.deleteCustomer(testCustomer.getId()), deletedRe);
+
+    }
+
+    /**
+     * Tests the get communication types method. Should return all of the 
+     * communication methods, followed by a string representation of that method. 
+     */
+    @Test
+    public void getCommunicationTypesTest() throws Exception {
      
-    //     Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
- 
-    //     given(customerController.createCustomer(testCustomer)).willReturn(Status.isOk);
- 
-    //     mvc.perform(post("/customers")
-    //         .contentType(MediaType.APPLICATION_JSON))
-    //         .andExpect(status().isOk());
-    // }
+        // Create a map of what the desired output should look like
+        Map<CommunicationType, String> testMap = new HashMap<CommunicationType, String>();
+        testMap.put(CommunicationType.EMAIL_AND_TEXT, "Email and Text");
+        testMap.put(CommunicationType.EMAIL, "Email");
+        testMap.put(CommunicationType.TEXT, "Text");
 
-    // @Test
-    // public void updateCustomerTest() throws Exception {
-     
-    //     Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
+        // Response Entity representation of the desired output
+        ResponseEntity<Map<CommunicationType, String>> re = ResponseEntity.ok(testMap);
 
-    //     Customer testCustomerUpdated = new Customer("Jane", "Doe", "jdoe@gmail.com", "336-344-0576", CommunicationType.TEXT, "990 Oval Dr, Raleigh, NC 27606");
- 
-    //     given(customerController.updateCustomer(testCustomer.getId(), testCustomerUpdated)).willReturn(Status.isOk);
- 
-    //     mvc.perform(put("/customers/1")
-    //         .contentType(MediaType.APPLICATION_JSON))
-    //         .andExpect(status().isOk());
-    // }
+        // Should return null when not instatiated.
+        assertNull(customerController.getCommunicationTypes());
 
-    // @Test
-    // public void deleteCustomerTest() throws Exception {
-     
-    //     Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
+        // Insantiate customer controller with a mock.
+        when(customerController.getCommunicationTypes()).thenReturn(re);
 
-    //     List<Customer> allCustomers = Arrays.asList(testCustomer);
+        // Ensure that the communication types return correctly. 
+        assertEquals(customerController.getCommunicationTypes(), re);
  
-    //     given(customerController.deleteCustomer(1)).willReturn(Status.isOk);
- 
-    //     mvc.perform(delete("/customers/1")
-    //         .contentType(MediaType.APPLICATION_JSON))
-    //         .andExpect(status().isOk());
-    // }
-
-    // @Test
-    // public void getCommunicationTypesTest() throws Exception {
-     
-    //     Customer testCustomer = new Customer("John", "Doe", "jdoe@gmail.com", "919-515-3000", CommunicationType.EMAIL_AND_TEXT, "890 Oval Dr, Raleigh, NC 27606");
-
-    //     List<Customer> allCustomers = Arrays.asList(testCustomer);
- 
-    //     given(customerController.getCommunicationTypes()).willReturn(allCustomers);
- 
-    //     mvc.perform(get("/customers/communicationType")
-    //         .contentType(MediaType.APPLICATION_JSON))
-    //         .andExpect(status().isOk())
-    //         .andExpect(jsonPath("$", hasSize(1)))
-    //         .andExpect(jsonPath("$[0].name", is(testCustomer.getFirstName())));
-    // }
+    }
 
 }
