f0d0c2d9f5d6fb63a3bf9f98e7f38559e1652c92
warose <warose@ncsu.edu>
2020-02-18 14:08:15 -0500

    Outline for the Assignment Controller Outline for the Assignment controller and setting up vs code

---
+++ server/src/main/java/bco/scheduler/controller/AssignmentController.java
@@ -0,0 +1,30 @@
+package bco.scheduler.controller;
+
+import javax.validation.Valid;
+
+import bco.scheduler.cron.NotificationSender;
+import bco.scheduler.model.TimeToSend;
+import bco.scheduler.repository.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import bco.scheduler.exception.ResourceNotFoundException;
+import bco.scheduler.model.Appointment;
+
+import java.util.List;
+import java.util.Map;
+
+import bco.scheduler.controller.AppointmentController.java;
+
+/**
+ * Assignment Controller
+ * @author Alex Rose
+ *
+ */
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "http://localhost:4200")
+public class AssignmentController(){
+
+}
